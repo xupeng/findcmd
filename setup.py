884572e2e94fbd43af160c40293841c67bbf23dc
Xupeng Yun <xupeng@xupeng.me>
2011-02-25 16:24:19 +0800

    Add long description and more keywords

--- setup.py
+++ setup.py
@@ -7,9 +7,30 @@ setup(name='findcmd',
       version=version,
       description="A command line tool for searching commands",
       long_description="""\
-This is a simple command line tool for searching through PATHs to find out commands contain certian keywords.""",
+This is a simple command line tool for searching through PATHs to find out commands contain certian keywords.
+
+Usage: findcmd [options] cmd
+
+Options:
+  -h, --help            show this help message and exit
+  -c, --case-sensitive
+  -e, --match-all
+  --no-color
+
+For example:
+xupeng@hopes ~ $ findcmd disk
+/sbin/auto**disk**mount
+/sbin/**disk**label
+/usr/bin/**disk**hits
+/usr/sbin/**disk**arbitrationd
+/usr/sbin/**disk**managementd
+/usr/sbin/**disk**tool
+/usr/sbin/**disk**util
+/usr/sbin/f**disk**
+/usr/sbin/p**disk**
+""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='command sysutils',
+      keywords='console command sysutils',
       author='Xupeng Yun',
       author_email='xupeng@xupeng.me',
       url='https://github.com/xupeng/findcmd',
